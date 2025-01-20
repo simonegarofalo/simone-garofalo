486ad6d6d3f14eab8348a86c186331cd10792d99
simonegarofalo <simonegarofalo96@gmail.com>
2025-01-20 21:59:23 +0100

    Added projets component

--- src/scripts/animation-hero.js
+++ src/scripts/animation-hero.js
@@ -1,10 +1,17 @@
 const textElement = document.querySelector(".dinamic-text-content");
-    const texts = ["web designer", "front-end developer", "creativo"];
+    const texts = ["web designer", "developer", "creativo"];
     let textIndex = 0;
 
+    function updateText() {
     textElement.textContent = texts[textIndex];
+    textElement.id = `text-${textIndex}`;
+    }
+
+    updateText();
 
     textElement.addEventListener("animationiteration", () => {
       textIndex = (textIndex + 1) % texts.length;
       textElement.textContent = texts[textIndex];
+
+      updateText();
     });
