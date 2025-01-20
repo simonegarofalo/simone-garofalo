f1924ef9383c106cb5b36fc3343c3365aa7c7a53
simonegarofalo <simonegarofalo96@gmail.com>
2025-01-20 11:38:50 +0100

    Updated cta

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,5 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  vite: {
-    server: {
-      headers: {
-        'Content-Type': 'text/html; charset=UTF-8',
-      },
-    },
-  },
   integrations: [tailwind()],
 });
