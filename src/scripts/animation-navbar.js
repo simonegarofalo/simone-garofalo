5eaabd7f87915fe690959ea1b671bd0001bb8b01
simonegarofalo <simonegarofalo96@gmail.com>
2025-01-22 16:56:52 +0100

    Connected section and navbar by a href

--- src/scripts/animation-navbar.js
+++ src/scripts/animation-navbar.js
@@ -2,14 +2,12 @@ const menu = document.getElementById("menu");
 const menuToggle = document.getElementById("menu-toggle");
 const menuClose = document.getElementById("menu-close");
 
-// Funzione per aprire il menu
 menuToggle.addEventListener("click", () => {
   menu.classList.remove("opacity-0", "-translate-y-full", "pointer-events-none");
   menu.classList.add("opacity-100", "translate-y-0");
   menuToggle.classList.add("hidden")
 });
 
-// Funzione per chiudere il menu
 menuClose.addEventListener("click", () => {
   menu.classList.remove("opacity-100", "translate-y-0");
   menu.classList.add("opacity-0", "-translate-y-full", "pointer-events-none");
