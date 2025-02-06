85a0c67e273897211358c94f7268e1f1e5515d43
simonegarofalo <simonegarofalo96@gmail.com>
2025-02-06 22:17:33 +0100

    updated weather component with night icon

--- src/scripts/weather.js
+++ src/scripts/weather.js
@@ -1,25 +1,30 @@
 const lat = 45.4643;
 const lon = 9.1895;
-const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true&timezone=Europe%2FRome`;
+const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true&hourly=temperature_2m,weathercode&timezone=Europe%2FRome`;
 
 fetch(url)
   .then(response => response.json())
   .then(data => {
-    // Estrai la temperatura attuale
     const currentTemperature = Math.floor(data.current_weather.temperature);
 
-    // Estrai il codice meteo attuale
     const currentWeatherCode = data.current_weather.weathercode;
+
+    const currentHour = new Date().getHours();
 
     const weatherImages = {
       'sereno': '/sunny.png',
       'nuvoloso': '/cloudy.png',
       'pioggia': '/rainy.png',
       'pioggia forte': '/stormy.png',
+      'notte': '/moon.png',
+
     };
 
     let weatherGroup;
 
+    if (currentHour >= 18 || currentHour < 6) {
+      weatherGroup = 'notte';
+    } else {
     if (currentWeatherCode === 0) {
       weatherGroup = 'sereno';
     } else if ([1, 2, 3].includes(currentWeatherCode)) {
@@ -33,10 +38,10 @@ fetch(url)
     } else {
       weatherGroup = 'condizione sconosciuta';
     }
+  }
 
     const weatherImage = weatherImages[weatherGroup] || './assets/sunny.png';
 
-    // Visualizza la temperatura e l'immagine
     document.getElementById('temperature').textContent = `${currentTemperature}°C`;
     document.getElementById('weather-img').src = weatherImage;
 
