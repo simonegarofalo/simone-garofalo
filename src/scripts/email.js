2129099eddf1f481322672a742ceaf0036c63630
simonegarofalo <simonegarofalo96@gmail.com>
2025-06-28 22:49:45 +0200

    Added send email function

---
+++ src/scripts/email.js
@@ -0,0 +1,24 @@
+document.querySelector("form").addEventListener("submit", async (e) => {
+    e.preventDefault();
+  
+    const name = document.querySelector("input[name='name']").value;
+    const email = document.querySelector("input[name='email']").value;
+  
+    const res = await fetch("/.netlify/functions/send-email", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ name, email })
+    });
+  
+    const data = await res.json();
+  
+    if (data.success) {
+      window.location.href = "/";
+    } else {
+      alert("Errore nell'invio della mail!");
+      console.error("Errore nell'invio della mail:", data.error);  }
+  });
+  
+    
