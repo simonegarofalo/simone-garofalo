44799ce3ba1fd3d79ac2270dfea62829dcdd960f
simonegarofalo <simonegarofalo96@gmail.com>
2025-06-29 16:55:24 +0200

    Updated form

--- src/scripts/email.js
+++ src/scripts/email.js
@@ -2,7 +2,7 @@ document.querySelector("form").addEventListener("submit", async (e) => {
     e.preventDefault();
   
     const email = document.querySelector("input[name='email']").value;
-    const message = document.querySelector("input[name='message']").value;
+    const message = document.querySelector("textarea[name='message']").value;
   
     const res = await fetch("/.netlify/functions/send-email", {
       method: "POST",
